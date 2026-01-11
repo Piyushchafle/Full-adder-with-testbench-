307cd4a7d905931da32a33596d563722cae0c79f
Piyush Chafle <piyushchafle45@gmail.com>
2026-01-11 23:50:45 +0530

    Create generator.sv

---
+++ generator.sv
@@ -0,0 +1,23 @@
+class generator;
+  transaction trans;
+  
+  mailbox gen2drv;
+  
+  function new (mailbox,gen2drv);
+    this.gen2drv=gen2drv;
+    
+  endfunction
+  
+  task main();
+    repeat(2)
+      begin
+        trans=new();
+        
+        trans.randomize;
+        
+        trans.display("generator class sinals");
+        
+        gen2drv.put(trans);
+      end
+   endtask
+  endmodule
